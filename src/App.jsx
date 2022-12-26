40f19620a25ab1c3750dcba04ab25b59591d5127
Josevargas1289 <joseroberto-vargas@hotmail.com>
2022-12-25 22:26:45 -0500

    proyecto terminado al 80%

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Clima from './components/Clima'
-import axios from 'axios'
+
 
 
 function App() {
